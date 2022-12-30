d99e4c5f2b9072408d7dcd9f209517286ca89cdc
pyogi37 <pyogi37@gmail.com>
2022-12-31 00:33:17 +0530

    setup post feed and shown all posts on home page with author name

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ console.log("Router loaded");
 
 router.get("/", homeController.home);
 router.use("/users", require("./users"));
+router.use("/posts", require("./posts"));
 
 module.exports = router;
