8e937add05b6d9e9a7aaec86221665dd1c93c041
pyogi37 <pyogi37@gmail.com>
2023-07-27 18:11:27 +0530

    resolved prod errors

--- config/environment.js
+++ config/environment.js
@@ -62,3 +62,8 @@ const production = {
 };
 
 module.exports = development;
+if (process.env.NODE_ENV === "production") {
+  module.exports = production;
+} else {
+  module.exports = development;
+}
